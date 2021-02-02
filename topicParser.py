3484ed4f1e3bb279545f4c811a058e6fb6ed61f9
Vysakh P Pillai <3634378+vppillai@users.noreply.github.com>
2021-02-02 09:27:02 +0530

    Update topicParser.py

--- topicParser.py
+++ topicParser.py
@@ -1,4 +1,4 @@
 import requests
 r=requests.get("https://api.github.com/repos/MicrochipTech/PIC32MZW1_Workshop/topics", headers={"Accept":"application/vnd.github.mercy-preview+json"})
-file = open("tags.json", "w")
+file = open("index.json", "w")
 file.write(r.text)
