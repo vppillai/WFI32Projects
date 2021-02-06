a5f2cb786da45fa4faa8592df648d97443ce74ab
Vysakh P Pillai <3634378+vppillai@users.noreply.github.com>
2021-02-06 13:00:16 +0530

    Update topicParser.py

--- topicParser.py
+++ topicParser.py
@@ -20,7 +20,7 @@ for mdpath in glob.iglob(os.path.join("docs", '*.md')):
 #Create the main readme
 file=open(os.path.join(".","docs","readme.md"),"w")
 file.write(f'<img align="left" width="100" height="100" src="logo.jpg">'+'\n\n')
-file.write(f'# Project topics in {orgName}'+'\n\n')    
+file.write(f'# Project topics in {orgName}'+'\n\n<br/><br/>')    
 for topic in topicsFilter:
     file.write(f'### [{topic}]({topic})' +'\n')
 
